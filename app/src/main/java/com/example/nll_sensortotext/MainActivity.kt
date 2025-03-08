705d746b35e875ee1a8176031d01f84d30a71711
kbs01 <kimbosung1217@gmail.com>
2025-03-08 17:32:24 +0900

    1. 디자인 변경 2. 불필요한 activity 삭제 3. main에서의 삭제된 activity 삭제

--- app/src/main/java/com/example/nll_sensortotext/MainActivity.kt
+++ app/src/main/java/com/example/nll_sensortotext/MainActivity.kt
@@ -16,21 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        var btnNavigate: Button = findViewById(R.id.btnNavigate)
-        var btnGyroSensor : Button = findViewById(R.id.자이로)
         var btnWifi : Button = findViewById(R.id.btnwifi)
         var btnSensorData : Button = findViewById(R.id.btnSensorData)
 
-        // 버튼 클릭 이벤트 처리
-        btnNavigate.setOnClickListener {
-            // 두 번째 화면으로 이동하는 Intent 생성
-            val intent = Intent(this, Acceleration::class.java)
-            startActivity(intent)
-        }
-        btnGyroSensor.setOnClickListener {
-            val intent = Intent(this, GyroSensorActivity::class.java)
-            startActivity(intent)
-        }
+
         btnWifi.setOnClickListener{
             val intent = Intent(this, Wifi::class.java)
             startActivity(intent)
